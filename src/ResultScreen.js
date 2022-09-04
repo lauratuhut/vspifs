927410a855b7f744293cdbf34c140a281b275ccf
Laura Tuhut <laura.tuhut@gmail.com>
2022-09-04 22:25:39 +0300

    use Gif component to render gifs

--- src/ResultScreen.js
+++ src/ResultScreen.js
@@ -1,3 +1,5 @@
+import { Gif } from "@giphy/react-components";
+
 function ResultScreen(props) {
   return (
     <>
@@ -22,18 +24,10 @@ function ResultScreen(props) {
           </div>
         </div>
       </nav>
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      <div className="mx-auto max-w-7xl px-2 py-6 sm:px-6 lg:px-8">
+        <div className="md:columns-5 gap-8">
           {props.results.map((item) => (
-            <a key={item.id} href={item.url} className="group">
-              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                <img
-                  src={item.images.fixed_height.url}
-                  alt={item.title}
-                  className="h-full w-full object-cover object-center group-hover:opacity-75"
-                />
-              </div>
-            </a>
+            <Gif gif={item} className="mb-6"/>
           ))}
         </div>
       </div>
